adca4e6845634442f2c944468a857a3e01b8c8fa
Peter <pelimimian@users.noreply.github.com>
2018-09-19 14:52:58 -0400

    Modified the scrolling postioning
    - Modified each section to scroll 200 px below the desired div section
    - Change the code format to increase readability.

--- index.js
+++ index.js
@@ -1,31 +1,23 @@
-
-$(document).ready(function()
-{                    
-  $("#PortfolioButton").click(function()
- {
-  $('html,body').animate({
-    scrollTop:$('#PortfolioInfo').offset() .top-135
-},500);
+$(document).ready(function(){                    
+  
+  $("#PortfolioButton").click(function(){
+      $('html,body').animate({
+        scrollTop:$('#PortfolioInfo').offset() .top+100},500);
   });
  
- $("#AboutButton").click(function()
- {
-  $('html,body').animate({
-    scrollTop:($('#AboutMeInfo').offset().top-135)
-},500);
+ $("#AboutButton").click(function(){
+      $('html,body').animate({
+        scrollTop:($('#AboutMeInfo').offset().top+100)},500);
   });
  
-$("#ContactButton").click(function()
- {
+$("#ContactButton").click(function(){
   $('html,body').animate({
-    scrollTop:($('#contactsSection').offset().top -100)
-},500);
+        scrollTop:($('#contactsSection').offset().top+200)},500);
   });
 
-$("#HomeButton").click(function()
- {
-  $('html,body').animate({
-    scrollTop:0 },500);
+$("#HomeButton").click(function(){
+    $('html,body').animate({
+      scrollTop:0 },500);
   });
-
+  
 });
